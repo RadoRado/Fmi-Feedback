3e50bcac7675d545aaed7c9d323c2dfd939e9ea1
Radoslav Georgiev <g2h.horde@gmail.com>
2011-09-04 18:13:52 +0300

    Refactored the main file a bit

--- javascript/main.js
+++ javascript/main.js
@@ -6,36 +6,44 @@ var FMIFeedback = {};
     FMIFeedback.ajaxSuggestRespSuggests = {};
     FMIFeedback.ajaxSuggestResp = [];
     FMIFeedback.imagePath = 'images/';
-
+    
+    FMIFeedback.icons = {
+        neutralColor :  "icon_neutral_color.png",
+        neutralGray :    "icon_neutral_gray.png",
+        happyColor : "icon_happy_color.png",
+        happyGray : "icon_happy_gray.png",
+        sadColor : "icon_sad_color.png",
+        sadGray : "icon_sad_gray.png"
+    };
 
     /* Autoloaders go here */
     $(document).ready(function(){
-	var imagePath = FMIFeedback.imagePath;
+        var imagePath = FMIFeedback.imagePath;
         $('.sad').not('.selected').click(function(event){
             $(restoreOthers(event.target)).parent().find('.radio[value="-1"]').attr('checked',true);
-            $(this).attr('src',imagePath+'icon_sad_color.png').addClass('selected');
+            $(this).attr('src',imagePath + FMIFeedback.icons["sadColor"]).addClass('selected');
         }).mouseover(function(){
-            $(this).not('.selected').attr('src',imagePath+'icon_sad_color.png');
+            $(this).not('.selected').attr('src',imagePath + FMIFeedback.icons["sadColor"]);
         }).mouseout(function(){
-            $(this).not('.selected').attr('src',imagePath+'icon_sad_gray.png');
+            $(this).not('.selected').attr('src',imagePath + FMIFeedback.icons["sadGray"]);
         });
 	
         $('.neutral').not('.selected').click(function(event){
             $(restoreOthers(event.target)).parent().find('.radio[value="0"]').attr('checked',true);
-            $(this).attr('src',imagePath+'icon_neutral_color.png').addClass('selected');
+            $(this).attr('src',imagePath + FMIFeedback.icons["neutralColor"]).addClass('selected');
         }).mouseover(function(){
-            $(this).not('.selected').attr('src',imagePath+'icon_neutral_color.png');
+            $(this).not('.selected').attr('src',imagePath + FMIFeedback.icons["neutralColor"]);
         }).mouseout(function(){
-            $(this).not('.selected').attr('src',imagePath+'icon_neutral_gray.png');
+            $(this).not('.selected').attr('src',imagePath + FMIFeedback.icons["neutralGray"]);
         });
 	
         $('.happy').click(function(event){
             $(restoreOthers(event.target)).parent().find('.radio[value="1"]').attr('checked',true);
-            $(this).attr('src',imagePath+'icon_happy_color.png').addClass('selected');
+            $(this).attr('src',imagePath + FMIFeedback.icons["happyColor"]).addClass('selected');
         }).mouseover(function(){
-            $(this).not('.selected').attr('src',imagePath+'icon_happy_color.png');
+            $(this).not('.selected').attr('src',imagePath + FMIFeedback.icons["happyColor"]);
         }).mouseout(function(){
-            $(this).not('.selected').attr('src',imagePath+'icon_happy_gray.png');
+            $(this).not('.selected').attr('src',imagePath + FMIFeedback.icons["happyGray"]);
         });
 	
         $('#anonymous').click(function(){
