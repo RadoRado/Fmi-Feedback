5fa9fd204064fd79f6d6ee5edd915871749a18a9
Radoslav Georgiev <g2h.horde@gmail.com>
2011-09-11 04:16:10 +0800

    Preparing the index for giving the questions to the template

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
-    header("Content-Type: text/html; charset=utf-8");
-    require_once("classes/Smarty.class.php");
-    
-    $smarty = new Smarty();
-    
-    $smarty->setTemplateDir("templates/");
-    $smarty->display("index.tpl");
+
+header("Content-Type: text/html; charset=utf-8");
+require_once("classes/Smarty.class.php");
+require_once("class_loader.php");
+require_once("config/db_config.php");
+
+$smarty = new Smarty();
+$database = new Database($db_config);
+
+$questions = array(); // array that will hold the questions
+// will move to class in the future
+$sql = "SELECT * FROM questions";
+$res = $database->query($sql);
+
+while ($row = $database->fetchObject($res)) {
+    $questions[$row->id] = $row->text;
+}
+
+
+$smarty->setTemplateDir("templates/");
+$smarty->assign("questions", $questions);
+
+$smarty->display("index.tpl");
 ?>
