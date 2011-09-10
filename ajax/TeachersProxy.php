b142199e2fd028994ce965b9739954574a7465bc
Radoslav Georgiev <g2h.horde@gmail.com>
2011-09-10 16:40:08 +0300

    The php file for the teachers

--- ajax/TeachersProxy.php
+++ ajax/TeachersProxy.php
@@ -3,7 +3,14 @@
 class TeachersProxy extends DatabaseAware {
 
     public function getTeachers($params = null) {
-        $sql = "SELECT * FROM teachers";
+        $sql = "SELECT * FROM teachers 
+                WHERE uid 
+                IN(
+                    SELECT teacher_id 
+                    FROM teacher_to_course 
+                    WHERE course_id = %d)";
+        
+        $sql = sprintf($sql, mysql_real_escape_string($params["courseId"]));
         $res = $this->database->query($sql);
 
         $resultArray = array();
