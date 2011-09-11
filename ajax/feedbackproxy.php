338137c655ee7434821a17716dba1e74a9d4a645
Radoslav Georgiev <g2h.horde@gmail.com>
2011-09-11 13:49:30 +0300

    Added else in the recursion and refactored the code to be more object oriented

--- ajax/feedbackproxy.php
+++ ajax/feedbackproxy.php
@@ -2,17 +2,8 @@
 
 class FeedbackProxy extends DatabaseAware {
 
-    private function _escapeParams(&$params) {
-        foreach ($params as $key => &$value) {
-            if (is_array($value)) {
-                $this->_escapeParams($value);
-            }
-            $value = $this->database->escape($value);
-        }
-    }
-
     public function sendFeedback($params = null) {
-        $this->_escapeParams($params);
+        $this->escapeParams($params);
     }
 
 }
--- classes/DatabaseAware.php
+++ classes/DatabaseAware.php
@@ -4,6 +4,16 @@ abstract class DatabaseAware {
 
     protected $database;
 
+    protected function escapeParams(&$params) {
+        foreach ($params as $key => &$value) {
+            if (is_array($value)) {
+                $this->escapeParams($value);
+            } else {
+                $value = $this->database->escape($value);
+            }
+        }
+    }
+
     public function __construct($database) {
         $this->database = $database;
     }
