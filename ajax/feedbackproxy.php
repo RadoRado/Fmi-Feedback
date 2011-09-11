94f00e2b84d9eab6bf0b0437ab1f4e9cd989c4e1
Radoslav Georgiev <g2h.horde@gmail.com>
2011-09-11 10:25:21 +0300

    Added recursive method for escaping. Test is coming soon

--- ajax/feedbackproxy.php
+++ ajax/feedbackproxy.php
@@ -1,8 +1,19 @@
 <?php
 
 class FeedbackProxy extends DatabaseAware {
-    public function sendFeedback($params = null) {
-        
+
+    private function _escapeParams(&$params) {
+        foreach ($params as $key => &$value) {
+            if (is_array($value)) {
+                $this->_escapeParams($value);
+            }
+            $value = $this->database->escape($value);
+        }
     }
+
+    public function sendFeedback($params = null) {
+        $this->_escapeParams($params);
+    }
+
 }
 
