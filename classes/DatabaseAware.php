8b11e9419d14b1a4296ece1c0d9d01184ea08609
Radoslav Georgiev <g2h.horde@gmail.com>
2011-09-12 02:00:59 +0300

    Some cleanup of unnecassary things

--- classes/DatabaseAware.php
+++ classes/DatabaseAware.php
@@ -3,20 +3,7 @@
 abstract class DatabaseAware {
 
     protected $database;
-
-    /*
-    I dont think we need this now
-	protected function escapeParams(&$params) {
-        foreach ($params as $key => &$value) {
-            if (is_array($value)) {
-                $this->escapeParams($value);
-            } else {
-                $value = $this->database->escape($value);
-            }
-        }
-    }
-	*/
-
+    
     public function __construct($database) {
         $this->database = $database;
     }
