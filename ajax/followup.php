c4ec84c1f890e111294f930347edd3ac082f6dcd
Radoslav Georgiev <g2h.horde@gmail.com>
2012-01-01 22:45:50 +0200

    Bug fix in one of the ajax proxies

--- ajax/followup.php
+++ ajax/followup.php
@@ -1,7 +1,7 @@
 <?php
 
 class FollowUp extends DatabaseAware {
-	public function count($params = null) {
+	public function count($model, $params = null) {
 		$sql = "INSERT INTO gamified(value, feedback_id) VALUES(?, ?)";
 		$value = ($params["gamified"] === "yes" ? 1 : 0);
 		$this -> database -> exec($sql, array($value, (int)$params["feedbackId"]));
