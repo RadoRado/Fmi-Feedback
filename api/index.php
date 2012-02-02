5c9b44483b261f775427951470758539a3cfd5f4
Radoslav Georgiev <g2h.horde@gmail.com>
2012-02-02 21:27:24 +0200

    Adding custom 404 and error pages for the API

--- api/index.php
+++ api/index.php
@@ -4,8 +4,25 @@ header('Content-type: application/json');
 require_once ("../include_me.php");
 require 'Slim/Slim.php';
 $app = new Slim();
-//GET route
 
+/**
+ * Set Custom 404 page when an API method is not found
+ */
+$app -> notFound(function() use ($app) {
+	$app -> render("api404.html");
+});
+
+/**
+ * Set Custom Error Page. No sensetive information should be displayed to the user
+ */
+$app -> error(function() use ($app) {
+	// log error
+	//$app -> render("apiError.html");
+});
+
+/**
+ * REST Methods
+ */
 $app -> get('/teacher/', function() use ($teacherModel) {
 	$result = $teacherModel -> get();
 	echo json_encode($result);
@@ -19,9 +36,9 @@ $app -> get('/course/', function() use ($courseModel) {
 $app -> get('/teacherByCourse/:id/', function($id) use ($teacherModel) {
 	$result = $teacherModel -> getByCourseId($id);
 	echo json_encode($result);
-});
+}) -> conditions(array("id" => '^[0-9]+$'));
 
-$app -> post("/followup/", function() use ($app, $gamifiedModel) {
+$app -> post("/followup/", function() use ($gamifiedModel) {
 	$value = ($_POST["gamified"] === "yes" ? 1 : 0);
 	$gamifiedModel -> create($value, $_POST["feedbackId"], $_POST["studentId"]);
 });
